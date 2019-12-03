fe90589cc7c87b6293b87a81072bba1e3735a5d2
vale5181 <58097866+vale5181@users.noreply.github.com>
2019-12-02 17:59:54 -0700

    Update projectDriver.cpp

--- projectDriver.cpp
+++ projectDriver.cpp
@@ -1,30 +1,66 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "project.hpp"
 
 using namespace std;
 
-int main()
+void mainmenu ()
 {
+  cout << "Select from the following" << endl;
+  cout << "-------------------------" << endl;
+  cout << "1. Display all group" << endl; // printTable()
+  cout << "2. Display members from a group" << endl; // printMembers()
+  cout << "3. Search an idol" << endl; //printIndividual
+  cout << "4. Add favorite group" << endl;
+  cout << "5. Add favorite idol" << endl;
+  cout << "6. Delete a group" << endl;
+  cout << "7. Log out" << endl;
+}
 
-  HashTable ht(10);
-
-  cout << "inserting group..." << endl;
-  ht.insertGroup("BTS","Jin","Vocalist","december 4 1992","179 cm","Jin is a very good cook");
-  ht.insertGroup("BTS","Jimin","Main vocalist","feb 4 2019","4 cm","Jimin likes Vananh");
-
-  ht.insertGroup("ATEEZ","San","Vocalist","dec 8 2018","170 cm","san likes butterflies");
-
-  if(ht.searchGroup("BTS"))
+int main(int argc, char* argv[])
+{
+  HashTable ht (15);
+  ifstream fp (argv[1]);
+  string s;
+  string groupN;
+  string memberN;
+  string position;
+  string bday;
+  string height;
+  string fact;
+  if (fp.is_open())
   {
-    cout << "found them" << endl;
-    ht.printMembers();
+
+    while (getline(fp,s))
+    {
+
+      stringstream ss(s);
+      getline(ss, groupN, ',');
+      getline(ss, memberN, ',');
+      getline(ss, position, ',');
+      getline(ss, bday, ',');
+      getline(ss, height, ',');
+      getline(ss, fact, ',');
+      cout << fact << endl;
+      ht.insertGroup(groupN, memberN, position, bday, height, fact);
+    }
+
+
   }
+
   else
   {
-    cout << "no jin ):" << endl;
+    cout << "Error." << endl;
+    return 0;
   }
-
+  cout << "---------------------------------------------------------------" << endl;
+  ht.printMembers("nct 127");
   ht.printTable();
-
-
+  ht.printIndividual("Jungkook");
+  ht.~HashTable();
+  ht.printTable();
+  ht.addFavGroup(1, "NCT 127");
+  ht.addFavGroup(1, "ATEEZ");
 }
