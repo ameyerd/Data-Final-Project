7f6c37b22a77224b03857238d429760362859925
Amey Erdenebileg <28586769+ameyerd@users.noreply.github.com>
2019-12-02 21:50:35 -0700

    Update driver2.cpp
    test

--- driver2.cpp
+++ driver2.cpp
@@ -27,8 +27,8 @@ int main(int argc, char* argv[])
       getline(ss, memberN, ',');
       getline(ss, position, ',');
       getline(ss, bday, ',');
-      getline(ss, height, ',');
-      getline(ss, fact, ',');
+      getline(ss, height,',');
+      getline(ss, fact,',');
       ht.insertGroup(groupN, memberN, position, bday, height, fact);
     }
 
@@ -41,7 +41,21 @@ int main(int argc, char* argv[])
     return 0;
   }
   cout << "---------------------------------------------------------------" << endl;
-  ht.printMembers();
-  ht.printTable();
+  // ht.printMembers();
+  //ht.printTable();
+  ht.printGroups();
+
+  ht.printIndividual("Mingi");
+
+  ht.addFavGroup(1, "NCT 127");
+  ht.addFavGroup(1, "Van");
+  ht.addFavGroup(2, "Twice");
+  ht.addFavGroup(3, "red velvet");
+  ht.addFavGroup(2, "day6");
+
+  ht.addFavMember(1, "yeri");
+  ht.addFavMember(2, "j-hope");
+  ht.addFavMember(1, "mark"); //this
+  ht.addFavMember(3, "joy");
 
 }
