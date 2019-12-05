5c311775c307771221bcef58390cf7602fdc1c9e
Amey Erdenebileg <28586769+ameyerd@users.noreply.github.com>
2019-12-05 16:38:50 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctime>
 #include "project.hpp"
 
 using namespace std;
@@ -113,6 +118,34 @@ group* HashTable::searchGroup(string groupName)
   return NULL;
 }
 
+member* HashTable::searchMember(string memberName)
+{
+
+    // cout << "hello member" <<endl;
+    for (int i =0; i < tableSize; i++)
+    {
+      group* n = table[i];
+
+      while(n != NULL)
+      {
+        member* m = n->head;
+        while (m != NULL)
+        {
+          if (toLowerdouble(memberName, m->memberName) == true)
+          {
+            return m;
+          }
+
+          m = m->n;
+        }
+
+        n = n->next;
+      }
+    }
+
+    return NULL;
+}
+
 bool HashTable::insertGroup(string groupName, string memberName, string position, string birthday, string height, string fact)
 {
   if(!searchGroup(groupName))
@@ -198,58 +231,66 @@ void HashTable::printTable()
 
 void HashTable::printGroups()
 {
-  int j =0;
+  int counter =0;
+  cout << endl;
+
+  cout <<  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ 🅚 🅟 🅞 🅟  🅖 🅡 🅞 🅤 🅟 🅢 ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << right << setw(16)<< endl;
   for(int i = 0; i < tableSize; i++)
   {
     group* n = table[i];
     while(n != NULL)
     {
-      // if(n->next == NULL)
-      // {
-      //   cout << n->groupName;
-      // }
-      // else
-      // {
-        if (j ==3)
+        if (counter == 3)
         {
-          cout << left << setw(20) << n->groupName << endl;
-          j =0;
+          cout << " ✭ " << left << setw(20) << n->groupName << right << setw(16) << endl;
+          counter =0;
         }
         else
         {
-          cout << right << setw(20) << n->groupName << " ミ✭ ";
-          j++;
+          cout << " ✭ " << left << setw(20) << n->groupName;
+          counter++;
         }
-      // }
       n = n->next;
     }
 
   }
   cout << endl;
+
+  //cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << endl;
 }
+
 
 void HashTable::printMembers(string groupN)
 {
 
-    group* n = searchGroup(groupN);
+  cout<<endl;
+  group* n = searchGroup(groupN);
 
-      cout << n->groupName << ": ";
+  cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ⋆⋅☆⋅⋆ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << right << setw(56) << endl;
+  cout <<  n->groupName << right << setw(47)<< endl;
+
+
+      // cout << n->groupName << ": ";
       member* m = n->head;
+      int counter = 0;
       while (m != NULL)
       {
-        if(m->n == NULL)
+        if(counter == 1)
         {
-          cout << m->memberName;
+          cout << " ♬♪ " << left << setw(10) << m->memberName <<  right << setw(47)<< endl;
+          counter = 0;
         }
         else
         {
-          cout << m->memberName << ", ";;
+          cout << " ♬♪ " << left << setw(10) << m->memberName;
+          counter++;
         }
 
         m = m->n;
       }
 
       cout << endl;
+      //cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ⋆⋅☆⋅⋆ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << endl;
 }
 
 void HashTable:: printIndividual(string memberN)
@@ -266,12 +307,16 @@ void HashTable:: printIndividual(string memberN)
       {
         if (toLowerdouble(memberN, m->memberName) == true)
         {
-          cout << "Group: "<< n->groupName << endl;
-          cout << "Name: " << m->memberName << endl;
-          cout << "Position: " << m->position << endl;
-          cout << "Birthday: " << m->birthday << endl;
-          cout << "Height: " << m->height << endl;
-          cout << "Fun Fact: " << m->fact << endl;
+
+          cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ⋆⋅☆⋅⋆ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" << right << setw(55) << endl;
+          cout << m->memberName << right << setw(17) << endl;
+
+          cout << "Group: "<< n->groupName << right << setw(20) << endl;
+          cout << "Position: " << m->position << right << setw(20) <<endl;
+          cout << "Birthday: " << m->birthday << right << setw(18) <<endl;
+          cout << "Height: " << m->height << right << setw(20) <<endl;
+          cout << "Fun Fact: " << m->fact << right << setw(20) <<endl;
+          cout << endl;
           return;
         }
 
@@ -279,11 +324,10 @@ void HashTable:: printIndividual(string memberN)
       }
 
       n = n->next;
-      cout << endl;
     }
   }
 
-  cout << "Idol not Found." << endl;
+  cout << "!🅴 🆁 🆁 🅾 🆁 ! Idol not Found." << endl;
 }
 
 void HashTable::addFavGroup(int rank, string groupName)
@@ -364,6 +408,173 @@ void HashTable::addFavMember(int rank, string memberName)
     }
   }
 
-  cout << "Idol not Found." << endl;
+  cout << "!🅴 🆁 🆁 🅾 🆁 ! Idol not Found." << endl;
 
+}
+
+//herlper function
+int randomizer(int num)
+{
+  return (rand() % num) + 1;
+}
+
+void HashTable:: quiz (string idolname)
+{
+  // cout << "start quiz " << endl;
+  if(searchMember(idolname))
+  {
+
+    cout << "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ⋆⋅☆⋅⋆ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"  << endl;
+    // cout << "founded" << endl;
+    member* right;
+    member* wrong;
+    int wrongchoice =-1;
+    for (int i =0; i < tableSize; i++)
+    {
+      group* n = table[i];
+
+      while(n != NULL)
+      {
+        member* m = n->head;
+        while (m != NULL)
+        {
+          if (toLowerdouble(idolname, m->memberName) == true)
+          {
+
+            while(wrongchoice == i || wrongchoice == -1)
+            {
+              wrongchoice = randomizer(tableSize);
+            }
+
+            right = m;
+          }
+
+          m = m->n;
+        }
+
+        n = n->next;
+      }
+    }
+  // cout << "wwrong choice" << wrongchoice << endl;
+    int num = randomizer(5);
+    // cout << "numm:" << num << endl;
+    int counter =0;
+    for (int i =0; i < wrongchoice; i++)
+    {
+      counter =0;
+      group* n = table[i];
+
+      while(n != NULL)
+      {
+        member* m = n->head;
+        while (m != NULL && counter != num)
+        {
+          wrong = m;
+          m = m->n;
+          counter++;
+        }
+
+        n = n->next;
+      }
+    }
+
+    int typeofquestion = 2;
+    int other = randomizer(2);
+    int taller = stoi(wrong->height);
+    string answer;
+
+    switch (typeofquestion)
+    {
+      case 1:
+        cout <<  "What is " << right->memberName << "'s height?"<< endl;
+
+        if(wrong->height == right->height)
+        {
+          taller = stoi(wrong->height) + 1;
+        }
+        if (other == 1)
+        {
+
+          cout << "a. " << right->height << endl;
+          cout << "b. " << taller  << endl;
+
+          cout << "Answer: ";
+          cin >> answer;
+
+          if (answer == "a" || answer == "A")
+          {
+            cout << "Correct! (:" << endl;
+          }
+          else
+          {
+            cout << "Wrong! ):<" << endl;
+          }
+        }
+
+        else
+        {
+          cout << "a. " << taller << endl;
+          cout << "b. " << right->height << endl;
+
+          cout << "Answer: ";
+          cin >> answer;
+
+          if (answer == "b" || answer == "B")
+          {
+            cout << "Correct! (:" << endl;
+          }
+          else
+          {
+            cout << "Wrong! ):<" << endl;
+          }
+        }
+
+
+        break;
+
+      case 2:
+        cout <<  "What is " << right->memberName << "'s birthday?"<< endl;
+
+        if (other == 1)
+        {
+
+          cout << "a. " << right->birthday << endl;
+          cout << "b. " << wrong->birthday << endl;
+          cout << "Answer: ";
+          cin >> answer;
+
+          if (answer == "a" || answer == "A")
+          {
+            cout << "Correct! (:" << endl;
+          }
+          else
+          {
+            cout << "Wrong! ):<" << endl;
+          }
+        }
+
+        else
+        {
+          cout << "a. " << wrong->birthday << endl;
+          cout << "b. " << right->birthday << endl;
+
+          cout << "Answer: ";
+          cin >> answer;
+
+          if (answer == "b" || answer == "B")
+          {
+            cout << "Correct! (:" << endl;
+          }
+          else
+          {
+            cout << "Wrong! ):<" << endl;
+          }
+        }
+        break;
+    }
+  }
+  else
+  {
+    cout << "!🅴 🆁 🆁 🅾 🆁 ! Idol not Found." << endl;
+  }
 }
